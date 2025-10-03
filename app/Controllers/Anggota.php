768666865af8efc50b98d5424559e7e24b27114d
marrelykasih <marrely.salsa.tif24@polban.ac.id>
2025-10-04 06:44:43 +0700

    mengganti anggota.php pada controllers

--- app/Controllers/Anggota.php
+++ app/Controllers/Anggota.php
@@ -1,5 +1,8 @@
 <?php namespace App\Controllers;
 
+use App\Models\AnggotaModel;
+use App\Models\PenggajianModel;
+
 class Anggota extends BaseController
 {
     public function index()
@@ -8,6 +11,20 @@ class Anggota extends BaseController
             return redirect()->to('/login');
         }
 
-        return view('anggota/index');
+        $id_pengguna = session()->get('id_pengguna');
+
+        $anggotaModel = new AnggotaModel();
+        $penggajianModel = new PenggajianModel();
+
+
+        $anggota = $anggotaModel->find($id_pengguna);
+
+        
+        $gaji = $penggajianModel->getGajiAnggota($id_pengguna);
+
+        return view('anggota/index', [
+            'anggota' => $anggota,
+            'gaji' => $gaji
+        ]);
     }
 }
